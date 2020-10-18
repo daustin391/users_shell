61d77b3038b668413c76025600da9781dea43a79
Dave Austin <d.austin_391@yahoo.com>
2020-10-18 19:01:23 -0400

    add template with table

--- users_app/views.py
+++ users_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Users
 
-# Create your views here.
+
+def index(request):
+    context = {"all_users": Users.objects.all()}
+    return render(request, "index.html", context)
