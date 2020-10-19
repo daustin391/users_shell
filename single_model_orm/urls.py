6966b888bb0ed2428328d15fc08fae673dca3692
Dave Austin <d.austin_391@yahoo.com>
2020-10-18 20:48:54 -0400

    added routing and form process

--- single_model_orm/urls.py
+++ single_model_orm/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
 from users_app import views
 
-urlpatterns = [
-    path("", views.index),
-]
+urlpatterns = [path("", views.index), path("add-user", views.add_user)]
--- users_app/views.py
+++ users_app/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Users
 
 
 def index(request):
     context = {"all_users": Users.objects.all()}
     return render(request, "index.html", context)
+
+
+def add_user(request):
+    if request.method == "POST":
+        Users.objects.create(
+            first_name=request.POST["first_name"],
+            last_name=request.POST["last_name"],
+            email_address=request.POST["email"],
+            age=request.POST["age"],
+        )
+        return redirect("/")
+
+    else:
+        return redirect("/")
